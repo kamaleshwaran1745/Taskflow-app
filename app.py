8b11d7f63e1537e98938836914db0e3059155257
Kamaleshwaran <warankamalesh587@gmail.com>
2026-04-09 22:10:10 +0530

    simplified app run

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def edit_task(task_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0',port=int(os.environ.get("PORT",5000)))
+    app.run()
